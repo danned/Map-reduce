0343eba658a993ca04fddded6dc00baab1fcbb97
daniel <daniel.dahlberg89@gmail.com>
2017-05-17 16:13:10 +0200

    Clean up of code and change timeout

--- page_rank.erl
+++ page_rank.erl
@@ -8,6 +8,7 @@
 -module(page_rank).
 -compile(export_all).
 -import(crawl, [find_urls/2]).
+%-import(lists,map/2).
 
 %% Use map_reduce to count word occurrences
 map(Url,ok) ->
@@ -20,24 +21,24 @@ reduce(Url,Ns) ->
     [{Url,lists:sum(Ns)}].
     
 
-page_rank() ->
+page_rank(_,_) ->
     dets:open_file(web,[{file,"web.dat"}]),
     Urls = dets:foldl(fun({K,_},Keys)->[K|Keys] end,[],web),
     map_reduce:map_reduce_seq(fun map/2, fun reduce/2,
                                     [{Url,ok} || Url <- Urls]).
 
 
-page_rank_distr() ->
+page_rank_distr(M,R) ->
    dets:open_file(web,[{file,"web.dat"}]),
    Urls = dets:foldl(fun({K,_},Keys)->[K|Keys] end,[],web),
-   map_reduce:map_reduce_par_dist(fun map/2, 64, fun reduce/2, 64,
+   map_reduce:map_reduce_dist(fun map/2, M, fun reduce/2, R,
                                     [{Url,ok} || Url <- Urls]).
 
-
-benchmark(F) ->
+% Takes input argument a 2-ary funtion, and the arguments
+benchmark(F,M,R) ->
     statistics(runtime),
     statistics(wall_clock),
-    F(),
+    F(M,R),
     {_, Time1} = statistics(runtime),
     {_, Time2} = statistics(wall_clock),
     U1 = Time1 * 1000,
@@ -45,6 +46,8 @@ benchmark(F) ->
     io:format("Code time=~p (~p) microseconds~n",
 [U1,U2]).
 
+% All nodes needs to open file before participating in worker pool
 open_file()->
     io:fwrite(user,"Opening file\n",[]),
-    dets:open_file(web,[{file,"web.dat"}]).
+    R = dets:open_file(web,[{file,"web.dat"}]),
+    io:fwrite("~w\n",[R]).
